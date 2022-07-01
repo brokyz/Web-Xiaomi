a96194940f1750d117e1a95dccfe3f4a863640f0
brokyz <brokyz@foxmail.com>
2022-07-01 16:15:04 +0800

    feat

---
+++ MyJS.js
@@ -0,0 +1,6 @@
+var btn = document.querySelector('.close');
+var student = document.querySelector('.student-info');
+
+btn.addEventListener('click', function(){
+    student.style.display = 'none';
+})
