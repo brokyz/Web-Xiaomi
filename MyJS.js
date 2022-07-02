15ceb188208ce11a2c84b60bb78119e73b7091b0
brokyz <brokyz@foxmail.com>
2022-07-02 15:11:46 +0800

    feat

--- MyJS.js
+++ MyJS.js
@@ -1,6 +1,7 @@
 var btn = document.querySelector('.close');
 var student = document.querySelector('.student-info');
-
+var shadow = student.previousElementSibling;
 btn.addEventListener('click', function(){
     student.style.display = 'none';
+    shadow.style.display = 'none';
 })
